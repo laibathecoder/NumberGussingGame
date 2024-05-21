eb78d5aa9778c9d173f9033155218372ed4d096b
laibathecoder <167254061+laibathecoder@users.noreply.github.com>
2024-05-22 03:08:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+//Number Guessing Game
+import inquirer from "inquirer";
+const computerMove = Math.floor(Math.random() * 5 + 1);
+const result = await inquirer.prompt([
+    {
+        name: "userMove",
+        type: "number",
+        message: "Please enter a number between 1 to 5",
+    },
+]);
+if (result.userMove === computerMove) {
+    console.log("Congratulation! You enter Correct Number :)");
+}
+else {
+    console.log("Sorry, you enter wrong number :(");
+}
